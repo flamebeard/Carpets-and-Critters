52f72666e778185c75211572d2bd7a3e73e30bbe
Ethan Hurst <ethan@hursty.co.nz>
2019-12-18 08:30:41 +1000

    Initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import InstagramFeed from './InstagramFeed'
+import './Footer.css'
+
+export default () => (
+  <div>
+    <h2 className="taCenter">
+      Follow us{' '}
+      <a href="https://instagram.com/ethanhurstyy/">@ethanhurstyy</a>
+    </h2>
+    <br />
+    <InstagramFeed count="8" />
+    <footer className="footer">
+      <div className="container taCenter">
+        <span>
+          © Copyright {new Date().getFullYear()} All rights reserved. Crafted by{' '} Hursty
+        </span>
+      </div>
+    </footer>
+  </div>
+)
